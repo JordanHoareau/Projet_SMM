08a38f39a89afaa6edf0e580e71b0bd65b36eff6
Jordan HOAREAU <j.hoareau2908@gmail.com>
2016-11-21 00:23:12 +0100

    updated

---
+++ Services_MM_Projet/www/scripts/photoSuccess.js
@@ -0,0 +1,54 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+    document.getElementById("imageCapture").addEventListener("click", imageCapture);
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+        var parentElement = document.getElementById('deviceready');
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+    };
+
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+
+    function imageCapture() {
+
+        var options = {
+            limit: 1
+        };
+
+        navigator.device.capture.captureImage(onSuccess, onError, options);
+
+        function onSuccess(mediaFiles) {
+            var i, path, len;
+
+            for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+                path = mediaFiles[i].fullPath;
+                console.log(mediaFiles);
+            }
+            window.location = "photoSuccess.html";
+        }
+
+        function onError(error) {
+            navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
+        }
+
+    }
+})();
---
+++ Services_MM_Projet/www/scripts/validateImage.js
@@ -0,0 +1,61 @@
+﻿// Pour obtenir une présentation du modèle Vide, consultez la documentation suivante :
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// Pour déboguer du code durant le chargement d'une page dans Ripple ou sur les appareils/émulateurs Android, lancez votre application, définissez des points d'arrêt, 
+// puis exécutez "window.location.reload()" dans la console JavaScript.
+(function () {
+    "use strict";
+
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+    document.getElementById("locationButton").addEventListener("click", getLocation);
+
+    function onDeviceReady() {
+        // Gérer les événements de suspension et de reprise Cordova
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+
+        // TODO: Cordova a été chargé. Effectuez l'initialisation qui nécessite Cordova ici.
+        var parentElement = document.getElementById('deviceready');
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+    };
+
+    function onPause() {
+        // TODO: cette application a été suspendue. Enregistrez l'état de l'application ici.
+    };
+
+    function onResume() {
+        // TODO: cette application a été réactivée. Restaurez l'état de l'application ici.
+    };
+
+    // onSuccess Callback
+    // This method accepts a Position object, which contains the
+    // current GPS coordinates
+    //
+    var getPosition = function (position) {
+        alert('Latitude: ' + position.coords.latitude + '\n' +
+              'Longitude: ' + position.coords.longitude + '\n');
+    };
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: ' + error.code + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+    function getLocation() {
+        alert("yo");
+        navigator.geolocation.getCurrentPosition(getPosition, onError);
+    }
+
+    function sendImage() {
+        var title = document.getElementById("imageForm").elements[0].value;
+        var comment = document.getElementById("imageForm").elements[1].value;
+        getLocation();
+        var locX = getPosition.coords.latitude;
+        var locY = getPosition.coords.longitude;
+        alert(locX);
+    }
+})();
